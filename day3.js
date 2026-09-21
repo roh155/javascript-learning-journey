245702099ebf8b6b40bb2e7e77f48a0d7948b3c6
roh155 <baheshwarrohit811@gmail.com>
2026-09-21 18:11:10 +0530

    Day 03: JavaScript arrays and loops logic

---
+++ day3.js
@@ -0,0 +1,13 @@
+let techStack = [ "HTML", "CSS", "JavaScript", "React", "Node.js"];
+
+console.log("My Current Tech Stack:");
+for(let i = 0; i<techStack.length; i++){
+    console.log(`- ${techStack[i]}`);
+}
+let targetMonths = 3;
+let CurrentMonth = 1;
+
+while (CurrentMonth <= targetMonths){
+    console.log(`Month ${CurrentMonth}: Practicing core web development daily.`);
+    CurrentMonth++;
+}
